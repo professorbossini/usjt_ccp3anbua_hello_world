703b77de39af410f7cea739a8fe0b2997e74e4b0
rodrigo <professorbossini@gmail.com>
2019-08-19 23:43:05 -0300

    Initial commit. Terminamos o pdf.

---
+++ app/src/main/java/br/com/bossini/usjt_ccp3anbua_hello_world/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.bossini.usjt_ccp3anbua_hello_world;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //infla a view
+        setContentView(R.layout.activity_main);
+    }
+}
